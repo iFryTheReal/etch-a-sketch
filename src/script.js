4bd0cec091ed7ee1001441f3f224e43c1100d995
iFryTheReal <93728868+iFryTheReal@users.noreply.github.com>
2023-07-10 15:30:41 +0200

    Add grid size edition

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 function makeGrid(size) {
   const canvas = document.getElementById("canvas");
 
+  canvas.replaceChildren(); // Remove all children elements (aka clear the grid)
+
   const rowModel = document.createElement("div");
   rowModel.classList.add("row");
 
@@ -20,39 +22,49 @@ function makeGrid(size) {
       row.append(cellModel.cloneNode());
     }
   }
+
+  colorEvents();
 }
 
-makeGrid(50);
+function colorEvents() {
+  const cells = document.getElementsByClassName("cell");
+
+  for (const cell of cells) {
+    cell.addEventListener("mouseover", (e) => {
+      if (!isDown) {
+        e.target.dataset.lastColor = `${e.target.style.backgroundColor}`;
+        e.target.style.backgroundColor = penColor;
+      } else {
+        e.target.dataset.lastColor = penColor;
+        e.target.style.backgroundColor = penColor;
+      }
+    });
+    cell.addEventListener("mouseout", (e) => {
+      e.target.style.backgroundColor = e.target.dataset.lastColor;
+    });
+    cell.addEventListener("mousedown", (e) => {
+      e.preventDefault(); // To prevent cell dragging
+      e.target.dataset.lastColor = penColor;
+      e.target.style.backgroundColor = penColor;
+    });
+  }
+}
 
 let penColor = "#000000";
 let isDown = false;
 
 const colorPicker = document.getElementById("color");
-const cells = document.getElementsByClassName("cell");
+const sizeInput = document.getElementById("grid-size");
 
 window.addEventListener("mousedown", (e) => (isDown = true));
 window.addEventListener("mouseup", () => (isDown = false));
 
-for (const cell of cells) {
-  cell.addEventListener("mouseover", (e) => {
-    if (!isDown) {
-      e.target.dataset.lastColor = `${e.target.style.backgroundColor}`;
-      e.target.style.backgroundColor = penColor;
-    } else {
-      e.target.dataset.lastColor = penColor;
-      e.target.style.backgroundColor = penColor;
-    }
-  });
-  cell.addEventListener("mouseout", (e) => {
-    e.target.style.backgroundColor = e.target.dataset.lastColor;
-  });
-  cell.addEventListener("mousedown", (e) => {
-    e.preventDefault(); // To prevent cell dragging
-    e.target.dataset.lastColor = penColor;
-    e.target.style.backgroundColor = penColor;
-  });
-}
-
+// Color choice
 colorPicker.addEventListener("change", (e) => {
   penColor = e.target.value;
 });
+
+// Grid size
+sizeInput.addEventListener("change", (e) => makeGrid(e.target.value));
+
+makeGrid(1);
