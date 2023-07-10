3848289aa44ce6e1aec315c003e7cc3b0affa24a
iFryTheReal <93728868+iFryTheReal@users.noreply.github.com>
2023-07-10 15:12:27 +0200

    Fix cell dragging bug

--- src/script.js
+++ src/script.js
@@ -6,6 +6,8 @@ function makeGrid(size) {
 
   const cellModel = document.createElement("div");
   cellModel.classList.add("cell");
+  cellModel.style.backgroundColor = "#ffffff";
+  cellModel.dataset.lastColor = "";
 
   for (let index = 0; index < size; index++) {
     canvas.append(rowModel.cloneNode());
@@ -20,4 +22,37 @@ function makeGrid(size) {
   }
 }
 
-makeGrid(5);
+makeGrid(50);
+
+let penColor = "#000000";
+let isDown = false;
+
+const colorPicker = document.getElementById("color");
+const cells = document.getElementsByClassName("cell");
+
+window.addEventListener("mousedown", (e) => (isDown = true));
+window.addEventListener("mouseup", () => (isDown = false));
+
+for (const cell of cells) {
+  cell.addEventListener("mouseover", (e) => {
+    if (!isDown) {
+      e.target.dataset.lastColor = `${e.target.style.backgroundColor}`;
+      e.target.style.backgroundColor = penColor;
+    } else {
+      e.target.dataset.lastColor = penColor;
+      e.target.style.backgroundColor = penColor;
+    }
+  });
+  cell.addEventListener("mouseout", (e) => {
+    e.target.style.backgroundColor = e.target.dataset.lastColor;
+  });
+  cell.addEventListener("mousedown", (e) => {
+    e.preventDefault(); // To prevent cell dragging
+    e.target.dataset.lastColor = penColor;
+    e.target.style.backgroundColor = penColor;
+  });
+}
+
+colorPicker.addEventListener("change", (e) => {
+  penColor = e.target.value;
+});
